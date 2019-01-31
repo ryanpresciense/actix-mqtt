d1bb7cb06eaccfbfc1987ad4bdc21958052d5771
Ryan Roberts <ryansroberts@gmail.com>
2019-01-31 11:30:19 +0000

    Resolve / Connect tcp

--- src/connector/mod.rs
+++ src/connector/mod.rs
@@ -1,32 +1,98 @@
-use actix::{Actor,Message,Context};
-use actix::actors::resolver::{Resolver,Resolve};
-use futures::{Poll};
-use tokio_io::{AsyncRead,AsyncWrite};
-use std::net::{IpAddr,Shutdown};
 use crate::iostream::IoStream;
+use actix::actors::resolver::{Resolve, Resolver, ResolverError};
+use actix::fut::{WrapFuture};
+use actix::{Actor, ActorResponse, Context, Handler, MailboxError, Message};
+use failure::Fail;
+use futures::{Future,  Poll};
+use std::collections::VecDeque;
 use std::io;
+use std::net::{IpAddr, Shutdown, SocketAddr};
 use std::time;
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_tcp::TcpStream;
 
 #[derive(Debug)]
-pub struct Connect {
-    to: Resolve,
+pub enum Connect {
+    Tcp(Resolve),
+}
+
+#[derive(Debug, Fail)]
+pub enum ConnectError {
+    #[fail(display = "Resolver")]
+    Resolver(#[cause] ResolverError),
+    #[fail(display = "Mailbox")]
+    Mailbox(#[cause] MailboxError),
+    #[fail(display = "Io")]
+    Io(#[cause] io::Error),
+    #[fail(display = "NsLookup")]
+    NsLookup,
+}
+
+impl From<io::Error> for ConnectError {
+    fn from(e: io::Error) -> Self {
+        ConnectError::Io(e)
+    }
+}
+
+impl Message for Connect {
+    type Result = Result<Connection, ConnectError>;
 }
 
 #[derive(Debug)]
-pub struct Connector {
+pub struct Connector {}
+
+impl Connector {
+    fn resolve(addr: Resolve) -> impl Future<Item = VecDeque<SocketAddr>, Error = ConnectError> {
+        let resolver = actix::System::current().registry().get::<Resolver>();
+        resolver
+            .send(addr)
+            .map_err(ConnectError::Mailbox)
+            .and_then(|x| x.map_err(ConnectError::Resolver))
+    }
+
+    fn connect(
+        addrs: VecDeque<SocketAddr>,
+    ) -> impl Future<Item = Connection, Error = ConnectError> {
+        TcpStream::connect(&addrs[0])
+            .map_err(ConnectError::Io)
+            .and_then(|connection| {
+                connection
+                    .peer_addr()
+                    .map(|peer| Connection::new(peer.into(), Box::new(connection)))
+                    .map_err(ConnectError::Io)
+            })
+    }
 }
 
 impl Actor for Connector {
     type Context = Context<Self>;
 }
 
+impl Handler<Connect> for Connector {
+    type Result = ActorResponse<Connector, Connection, ConnectError>;
+
+    fn handle(&mut self, connect: Connect, _ctx: &mut Self::Context) -> Self::Result {
+        match connect {
+            Connect::Tcp(resolve) => ActorResponse::r#async(
+                Self::resolve(resolve)
+                    .and_then(Self::connect)
+                    .into_actor(self),
+            ),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum ConnectionName {
-    HostAndPort(IpAddr,u16),
-    Other(String)
+    HostAndPort(IpAddr, u16),
+    Other(String),
 }
 
+impl From<SocketAddr> for ConnectionName {
+    fn from(addr: SocketAddr) -> Self {
+        ConnectionName::HostAndPort(addr.ip(), addr.port())
+    }
+}
 
 pub struct Connection {
     name: ConnectionName,
@@ -40,19 +106,19 @@ impl ::std::fmt::Debug for Connection {
 }
 
 impl Connection {
-    fn new(name: ConnectionName,stream: Box<IoStream + Send>) -> Self {
-        Connection {
-            name,
-            stream
-        }
+    fn new(name: ConnectionName, stream: Box<IoStream + Send>) -> Self {
+        Connection { name, stream }
     }
 
     pub fn stream(&mut self) -> &mut IoStream {
         &mut *self.stream
     }
 
-    pub fn from_stream<T: IoStream + Send,N: AsRef<str>>(name: N,io: T) -> Connection {
-        Connection::new(ConnectionName::Other(name.as_ref().to_owned()), Box::new(io))
+    pub fn from_stream<T: IoStream + Send, N: AsRef<str>>(name: N, io: T) -> Connection {
+        Connection::new(
+            ConnectionName::Other(name.as_ref().to_owned()),
+            Box::new(io),
+        )
     }
 }
 
